7b409d4d08f3dd36fd3f4872ef8432a15bfa2688
algerkong <1455048564@qq.com>
2019-04-27 20:23:55 +0800

    0427

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*头部应用打开关闭*开始**/
+function bd(){
+			var gs = document.getElementsByClassName("gyy")[0];
+			var sjx = document.getElementsByClassName("sj-x")[0];
+			var sjs = document.getElementsByClassName("sj-s")[0];
+			gs.className = "ggg"; 
+			sjx.className = "sjx";
+			sjs.className = "sjs";
+		}
+		
+function gx(){
+			var gs = document.getElementsByClassName("ggg")[0];
+			var sjx = document.getElementsByClassName("sjx")[0];
+			var sjs = document.getElementsByClassName("sjs")[0];
+			gs.className = "gyy"; 
+			sjx.className = "sj-x";
+			sjs.className = "sj-s";
+		}
+		
+/*头部应用打开关闭*结束**/
+
+/*你的建议打开关闭*开始**/
+function my(){
+			var you = document.getElementsByClassName("jy1")[0];
+			you.className = "jy";
+		}
+		
+function xs(){
+					var you = document.getElementsByClassName("jy")[0];
+					you.className = "jy1";
+		}
+/*你的建议打开关闭*结束**/
